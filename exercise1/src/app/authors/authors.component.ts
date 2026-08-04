1ddf5fe2359995adae96450b013785a516d90932
giochajon <giochajon@gmail.com>
2026-08-04 14:17:21 -0600

    Upgrade exercise1 Angular 18 -> 19 via ng update (standalone:false migration on non-standalone components)

--- exercise1/src/app/authors/authors.component.ts
+++ exercise1/src/app/authors/authors.component.ts
@@ -2,17 +2,17 @@ import { AuthorServiceService } from './../author-service.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-authors',
-  //templateUrl: './authors.component.html',
-  styleUrls: ['./authors.component.css'],
-  template:`
+    selector: 'app-authors',
+    //templateUrl: './authors.component.html',
+    styleUrls: ['./authors.component.css'],
+    template: `
   <ul>
   <li *ngFor="let author of authors">
    {{ author }}
   </li>
   </ul>
-  `
-
+  `,
+    standalone: false
 })
 export class AuthorsComponent  {
   authors; //= ['auth1','auth2','auth3','auth4']
